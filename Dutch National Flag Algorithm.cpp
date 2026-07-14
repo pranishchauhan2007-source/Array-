b1c6c1b246e9e7dd8365d07374729d7e911e8d5d
pranishchauhan2007-source <pranishchauhan2007@gmail.com>
2026-07-14 22:22:42 +0530

    Dutch national flag algorithm

---
+++ Dutch National Flag Algorithm.cpp
@@ -0,0 +1,36 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int arr[] = {1,2,0,1,1,0,2};
+    int n = sizeof(arr)/sizeof(arr[0]);
+
+    int low = 0;
+    int mid = 0;
+    int high = n - 1;
+
+    while(mid <= high)
+    {
+        if(arr[mid] == 0)
+        {
+            swap(arr[low], arr[mid]);
+            low++;
+            mid++;
+        }
+        else if(arr[mid] == 1)
+        {
+            mid++;
+        }
+        else
+        {
+            swap(arr[mid], arr[high]);
+            high--;
+        }
+    }
+
+    for(int i = 0; i < n; i++)
+    {
+        cout << arr[i] << " ";
+    }
+}

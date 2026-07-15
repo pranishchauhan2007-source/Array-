644fc01b495531e4e3b403f0b331447bbc2e0a60
pranishchauhan2007-source <pranishchauhan2007@gmail.com>
2026-07-15 21:57:18 +0530

    Max Consecutive ones

---
+++ Max Consecutive Ones.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+ int arr[]={1,0,1,1,1,1,1,0,1,1,1,1};
+ int n=sizeof(arr)/sizeof(arr[0]);
+ int count=0;
+ int maxcount=0;
+ for(int k=0; k<n; k++)
+ {
+  if(arr[k]==1)
+  {
+  count++;
+  if(count>maxcount)
+  {
+  maxcount=count;
+  }
+  }
+  else
+  count= 0;
+ }
+ cout<<maxcount;
+}

f00449a59eae25606db00acbcabe8e15822b499c
pranishchauhan2007-source <pranishchauhan2007@gmail.com>
2026-07-16 00:26:43 +0530

    Building with ocean view

---
+++ Buildings With Ocean View.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int sun[] = {4,2,6,18,5,7,12,6};
+    int n = sizeof(sun)/sizeof(sun[0]);
+
+    int count = 0;
+    int maxHeight = 0;
+
+    for(int i=n-1; i>=0; i--)
+    {
+        if(sun[i] > maxHeight)
+        {
+            count++;
+            //cout << i << " ";
+            maxHeight = sun[i];
+        }
+    }
+
+    cout << "\nCount: " << count;
+
+    return 0;
+}

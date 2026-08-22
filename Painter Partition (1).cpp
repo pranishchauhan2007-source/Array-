20bbeb28a1a4d4591f09c0d1c8c1b6ff559f5857
pranishchauhan2007-source <pranishchauhan2007@gmail.com>
2026-08-22 22:05:25 +0530

    Painter partition

---
+++ Painter Partition (1).cpp
@@ -0,0 +1,41 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+ int arr[]={12,34,67,90};
+ int N=4;
+ int M=2;
+ if(M>N)
+ cout<<-1;
+ int start=0, end=0, mid, ans;
+ for(int i=0; i<N; i++)
+ {
+  if(start<arr[i])
+  start =arr[i];
+  end+=arr[i];
+ }
+ while(start<= end)
+ {
+   mid= start+(end- start)/2;
+   int time=0, count=1;
+   for(int i=0;i<N;i++)
+   {
+     time+=arr[i];
+     if(time>mid)
+     {
+      count++;
+      time= arr[i];
+     }
+   }
+   if(count <= M)
+   {
+    ans= mid;
+    end= mid-1;
+   }
+   else
+   start=mid+1;
+ }
+ 
+ cout<< ans;
+
+}
